0361a2d1a4e4190f0080631f6478df2067ecc3dd
Noah <23338687+enjay27@users.noreply.github.com>
2021-11-19 13:59:20 +0900

    init new spring boot project

---
+++ src/test/java/com/athome/feel/FeelAtHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athome.feel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelAtHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
